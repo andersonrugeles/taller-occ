72c1ad648b3e6622c291edf24d885d29101d8be0
andersonrugeles <lrujeles@pratechgroup.com>
2023-10-20 11:37:40 -0500

    change endopoint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import { useSession, signIn, signOut } from "next-auth/react";
-import fetch from "isomorphic-fetch"
+import fetch from "isomorphic-fetch";
 import { UserSwitchOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import ProductList from "@/components/ProductList/ProductList";
 import LayoutComponent from "@/components/Layout/Layout";
 
-/* export const getStaticProps = async () => {
-  const response = await fetch("http://localhost:3000/api/products");
+export const getStaticProps = async () => {
+  const response = await fetch("https://taller-occ.vercel.app/api/products");
   const { data: productList } = await response.json();
 
   return {
@@ -16,7 +16,7 @@ import LayoutComponent from "@/components/Layout/Layout";
       productList,
     },
   };
-}; */
+};
 
 export default function Home({ productList }) {
   const { data: session } = useSession();
@@ -24,7 +24,7 @@ export default function Home({ productList }) {
   if (session) {
     return (
       <LayoutComponent>
-        <ProductList products={[]} />
+        <ProductList products={productList} />
       </LayoutComponent>
     );
   }
