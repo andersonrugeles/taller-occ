c8c6b848dd3ea8859c2a01cba8c370928d162504
andersonrugeles <lrujeles@pratechgroup.com>
2023-10-20 11:53:23 -0500

    add secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,15 +1,15 @@
-
-import NextAuth from "next-auth"
-import CognitoProvider from "next-auth/providers/cognito"
+import NextAuth from "next-auth";
+import CognitoProvider from "next-auth/providers/cognito";
 
 export const authOptions = {
-    providers: [
-        CognitoProvider({
-            clientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
-            clientSecret: process.env.NEXT_PUBLIC_COGNITO_CLIENT_SECRET,
-            issuer: process.env.NEXT_PUBLIC_COGNITO_ISSUER,
-        }),
-    ]
-}
+  providers: [
+    CognitoProvider({
+      clientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_COGNITO_CLIENT_SECRET,
+      issuer: process.env.NEXT_PUBLIC_COGNITO_ISSUER,
+    }),
+  ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
